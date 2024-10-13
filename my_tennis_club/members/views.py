63588e51628798c32f11af14c5fecea81f890673
John Jorel Landicho <152581325+jtlandicho@users.noreply.github.com>
2024-10-13 19:39:58 +0000

    deleted first html

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -2,7 +2,6 @@ from django.http import HttpResponse
 from django.template import loader
 
 def members(request):
-  template = loader.get_template('myfirst.html')
   template = loader.get_template('indescribable.html')
   return HttpResponse(template.render())
 
