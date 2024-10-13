413b721a2d49ac3c3a8e5dd641870094f7c7f6b6
John Jorel Landicho <152581325+jtlandicho@users.noreply.github.com>
2024-10-13 19:39:21 +0000

    edited indescribable

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -3,8 +3,6 @@ from django.template import loader
 
 def members(request):
   template = loader.get_template('myfirst.html')
+  template = loader.get_template('indescribable.html')
   return HttpResponse(template.render())
 
-def members2(request):
-  template = loader.get_template('indescribibable.html')
-  return HttpResponse(template.render())
