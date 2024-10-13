93ec089e5a44f7ba5b0e7db2641547a2ad253406
John Jorel Landicho <152581325+jtlandicho@users.noreply.github.com>
2024-10-13 19:37:39 +0000

    added html file

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -4,3 +4,7 @@ from django.template import loader
 def members(request):
   template = loader.get_template('myfirst.html')
   return HttpResponse(template.render())
+
+def members2(request):
+  template = loader.get_template('indescribibable.html')
+  return HttpResponse(template.render())
